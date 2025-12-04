2aee8a980f228eea635f5fa8f35f380db98a4569
Your Name <you@example.com>
2025-12-04 22:11:32 +0200

    fix: remove base href and use absolute paths in bootstrap config

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -34,10 +34,11 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c34c","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c34c","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"/vantabuddy-flutter/main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
     serviceWorkerVersion: "178550543"
-  }
+  },
+  entrypointBaseUrl: "/vantabuddy-flutter/"
 });
